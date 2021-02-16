8e1edbc7d6c84905246bd33c6d7b26e74b2ff7c5
harashimahashi <harashimahashi@gmail.com>
2021-02-16 19:21:22 +0200

    improved symtab

--- include/llvmc/isymbols.h
+++ include/llvmc/isymbols.h
@@ -9,16 +9,16 @@ namespace llvmc {
 
         class Env {
 
-            std::unordered_map<std::string, std::shared_ptr<inter::Id>> table_;
+            std::unordered_map<std::string_view, std::shared_ptr<inter::Id>> table_;
             std::shared_ptr<Env> prev_;
 
         public:
 
             Env(std::shared_ptr<Env>);
 
-            void insert(std::string, std::shared_ptr<inter::Id>);
-            std::shared_ptr<inter::Id> get(std::string);
-            std::shared_ptr<inter::Id> get_current(std::string);
+            void insert(std::string_view, std::shared_ptr<inter::Id>);
+            std::shared_ptr<inter::Id> get_current(std::string_view);
+            std::shared_ptr<inter::Id> get(std::string_view);
 
         };
     }
--- src/symbols.cpp
+++ src/symbols.cpp
@@ -4,27 +4,27 @@ namespace llvmc {
 
     namespace symbols {
 
-        Env::Env(std::shared_ptr<Env> n) : prev_{ n } {}
-        void Env::insert(std::string l, std::shared_ptr<inter::Id> i) {
+        Env::Env(std::shared_ptr<Env> n) : prev_{ std::move(n) } {}
+        void Env::insert(std::string_view l, std::shared_ptr<inter::Id> i) {
             
-            table_.emplace(std::move(l), i);
+            table_.emplace(std::move(l), std::move(i));
         }
-        std::shared_ptr<inter::Id> Env::get(std::string t) {
-
-            for(Env* e = this; e != nullptr; e = e->prev_.get()) {
-                auto found = e->table_.find(t);
-                if(found != e->table_.end()) return found->second; 
-            }
-            return nullptr;
-        }
-        std::shared_ptr<inter::Id> Env::get_current(std::string t) {
+        std::shared_ptr<inter::Id> Env::get_current(std::string_view t) {
 
             auto found = table_.find(t);
-            
             if(found != table_.end()) return found->second;
 
             return nullptr;
         }
+        std::shared_ptr<inter::Id> Env::get(std::string_view t) {
 
+            for(Env* e = this; e != nullptr; e = e->prev_.get()) {
+                
+                auto ret = e->get_current(t);
+                if(ret) return ret;
+            }
+
+            return nullptr;
+        }
     }
 }
